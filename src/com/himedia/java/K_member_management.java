77f740d300544702dc324cac8b6357ea27397d6d
someone <someone@someplace.com>
2024-08-26 17:22:54 +0900

    회원관리

--- src/com/himedia/java/K_member_management.java
+++ src/com/himedia/java/K_member_management.java
@@ -115,6 +115,49 @@ public class K_member_management {
         }
     }
 
+    public static void updateMember(String[][] members){
+        //1.이메일 조회 -> 사용자로부터 이메일을 받아온다
+        Scanner sc = new Scanner(System.in);
+        System.out.println("[수정]이메일을 입력해주세요.");
+        String email = sc.nextLine();
+        int idx=-1;
+        //2.조회
+        for(int i = 0; i < members.length; i++){
+            if(email.equals(members[i][1])){
+               idx=i;
+               break;
+            }
+        }
+        //3.찾았으면 해당 인덱스 값을 가지고 있는다
+        //3-1.찾지 못했으면 "찾으시는 회원이 없습니다"를 출력 후 updateMember 함수 종료
+        if(idx == -1){
+            System.out.println("찾으시는 회원이 없습니다");
+            return;
+        }
+        //4.새로운 이름, 이메일, 연락처 입력받는다
+        System.out.println("변경할 이름을 입력하세요");
+        String newName = sc.nextLine();
+        System.out.println("변경할 이메일을 입력하세요");
+        String newEmail = sc.nextLine();
+        System.out.println("변경할 연락처를 입력하세요");
+        String newPhone = sc.nextLine();
+        //5.찾은 인덱스의 값을 갱신한다
+        members[idx][0] = newName;
+        members[idx][1] = newEmail;
+        members[idx][2] = newPhone;
+
+        System.out.println("수정이 완료되었습니다");
+
+    }
+
+    public static void deleteMember(String[][] members){
+        //1.이메일로 삭제할 회원 찾는다
+        //2.찾은 행정보값을 별도로 가지고 있는다
+        //2-1.찾지 못했으면 안내 문구 출력 후 종료
+        //3.이후 찾은 행들을 모두 땡긴 후 마지막행을 null 처리한다
+
+    }
+
 
     public static void main(String[] args) {
         //사용자로 부터 요금제 선택 받아서
@@ -142,6 +185,7 @@ totalCnt=pricePlanNum*10;
                 selectAll(members);
                 break;
                 case 5:
+                  updateMember(members);
                 break;
                  case 6:
                  break;
---
+++ src/com/himedia/practice/K_member_management.java
@@ -0,0 +1,15 @@
+package com.himedia.practice;
+
+import java.util.Scanner;
+
+public class K_member_management {
+
+    public static int printPricePlan(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter price plan");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
