6aff8515600cdd5905944627ccc3a55e94db5c0a
someone <someone@someplace.com>
2024-08-27 17:16:03 +0900

    은행 계좌

--- src/com/himedia/practice/kMemberManagement.java
+++ src/com/himedia/practice/kMemberManagement.java
@@ -2,7 +2,7 @@ package com.himedia.java;
 
 import java.util.Scanner;
 
-public class K_member_management {
+public class kMemberManagement {
 
     static int totalCnt = 0;
     static int totalMemberCnt = 0;
@@ -116,32 +116,29 @@ public class K_member_management {
     }
 
     public static void updateMember(String[][] members){
-        //1.이메일 조회 -> 사용자로부터 이메일을 받아온다
         Scanner sc = new Scanner(System.in);
-        System.out.println("[수정]이메일을 입력해주세요.");
-        String email = sc.nextLine();
+        System.out.println("[수정]이메일을 입력해주세요");
+        String name = sc.nextLine();
         int idx=-1;
-        //2.조회
         for(int i = 0; i < members.length; i++){
-            if(email.equals(members[i][1])){
-               idx=i;
-               break;
+            if(members[i][0].equals(name)){
+                idx = i;
+                break;
             }
         }
-        //3.찾았으면 해당 인덱스 값을 가지고 있는다
-        //3-1.찾지 못했으면 "찾으시는 회원이 없습니다"를 출력 후 updateMember 함수 종료
-        if(idx == -1){
+
+        if(idx != -1){
             System.out.println("찾으시는 회원이 없습니다");
             return;
         }
-        //4.새로운 이름, 이메일, 연락처 입력받는다
+
         System.out.println("변경할 이름을 입력하세요");
         String newName = sc.nextLine();
         System.out.println("변경할 이메일을 입력하세요");
         String newEmail = sc.nextLine();
         System.out.println("변경할 연락처를 입력하세요");
         String newPhone = sc.nextLine();
-        //5.찾은 인덱스의 값을 갱신한다
+
         members[idx][0] = newName;
         members[idx][1] = newEmail;
         members[idx][2] = newPhone;
@@ -151,34 +148,12 @@ public class K_member_management {
     }
 
     public static void deleteMember(String[][] members){
-        //1.이메일로 삭제할 회원 찾는다
-        //2.찾은 행정보값을 별도로 가지고 있는다
-        //2-1.찾지 못했으면 안내 문구 출력 후 종료
-        //3.이후 찾은 행들을 모두 땡긴 후 마지막행을 null 처리한다
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("[삭제]이메일을 입력하세요.");
-        int idx=-1;
-        for(int i = 0; i < members.length; i++){
-            if(members[i][0].equals(members[i][1])){
-                idx=i;
-                break;
-            }
-        }
+        System.out.println("[삭제]이메일을 입력하세요");
+        String name = sc.nextLine();
 
-        if(idx == -1){
-            System.out.println("찾으시는 회원이 없습니다.");
-        }
 
-        members[idx][0] = null;
-        members[idx][1] = null;
-        members[idx][2] = null;
-
-        for(int i = idx; i < members.length-1; i++){
-            members[i][0] = members[i+1][0];
-            members[i][1] = members[i+1][1];
-            members[i][2] = members[i+1][2];
-        }
-        totalMemberCnt--;
     }
 
 
@@ -189,7 +164,7 @@ public class K_member_management {
         //배열명: members
         int pricePlanNum = printPricePlan();
         String[][] members = new String[pricePlanNum*10][3];
-totalCnt=pricePlanNum*10;
+        totalCnt=pricePlanNum*10;
         while(true){
             //7번 프로그램 종료
             //"이용해주셔서 감사합니다"
@@ -197,29 +172,29 @@ totalCnt=pricePlanNum*10;
             switch(menuNum){
                 case 1:
                     addMember(members);
-                 break;
-                 case 2:
-                     selectEmail(members);
-                break;
-                 case 3:
-                     selectName(members);
-                break;
+                    break;
+                case 2:
+                    selectEmail(members);
+                    break;
+                case 3:
+                    selectName(members);
+                    break;
                 case 4:
-                selectAll(members);
-                break;
+                    selectAll(members);
+                    break;
                 case 5:
-                  updateMember(members);
-                break;
-                 case 6:
-                     deleteMember(members);
-                 break;
-                 case 7:
-                 System.out.println("이용해주셔서 감사합니다.");
-                 return;
+                    updateMember(members);
+                    break;
+                case 6:
+                    deleteMember(members);
+                    break;
+                case 7:
+                    System.out.println("이용해주셔서 감사합니다.");
+                    return;
 
                 default:
                     System.out.println("잘못 선택하셨습니다.");
-                  break;
+                    break;
             }
 
         }
